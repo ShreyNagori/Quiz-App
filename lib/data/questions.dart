206df9994ea45b060ddfa71f728a919c3c11555c
ShreyNagori <sh3716@srmist.edu.in>
2023-06-18 17:45:57 +0530

    Quiz app

---
+++ lib/data/questions.dart
@@ -0,0 +1,93 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'Today it is Thursday.After 132 days,it will be:',
+    [
+      'Wednesday',
+      'Monday',
+      'Sunday',
+      'Tuesday',
+    ],
+  ),
+  QuizQuestion(
+      'Devesh can cover a certain distance in 1 hour 24 minutes by covering two-third of the distance at 6 km/hour and the rest at 7 km/hr. Calculate total distance.',
+      [
+        '6 KM',
+        '9 KM',
+        '18 KM',
+        '4 KM',
+      ]),
+  QuizQuestion(
+    'Find the H.C.F, if the numbers are in the ratio of 4 : 5 : 6 and their L.C.M. is 2400.',
+    [
+      '20',
+      '35',
+      '67',
+      '45',
+    ],
+  ),
+  QuizQuestion(
+    'What is the value of c , If 8 is 4% of a, and 4 is 8% of b. c equals b/a?',
+    [
+      '1/4',
+      '2',
+      'None of These',
+      '8',
+    ],
+  ),
+  QuizQuestion(
+    'The rate at which a sum becomes four times of itself in 25 years at S.I.,will be:',
+    [
+      '12%',
+      '23%',
+      '9%',
+      '25%',
+    ],
+  ),
+  QuizQuestion(
+    'If 9th of the month falls on the day preceding Sunday, then on what day will 1st of the month fall?',
+    [
+      'Friday',
+      'Saturday',
+      'Sunday',
+      'Monday',
+    ],
+  ),
+  QuizQuestion(
+    'The average of five numbers is 27. If one number is excluded, the average becomes 25. The excluded number is ?',
+    [
+      '35',
+      '25',
+      '45',
+      '55',
+    ],
+  ),
+  QuizQuestion(
+    'Fresh fruit contains 68% water and dry fruit contains 20% water. How much dry fruit can be obtained from 100 kg of fresh fruits ?',
+    [
+      '40',
+      '20',
+      '50',
+      'None of the above',
+    ],
+  ),
+  QuizQuestion(
+    'A sum of money placed at compound interest doubles itself in 4 years. In how many years will it amount to 8 times?',
+    [
+      '12',
+      '8',
+      '9',
+      '27',
+    ],
+  ),
+  QuizQuestion(
+    'Half percent, written as a decimal, is?',
+    [
+      '0.005',
+      '0.05',
+      '0.5',
+      '0.55',
+    ],
+  ),
+];
